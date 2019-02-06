0decc7c39595685453e42174b74ff4f12b78c97a
Jason Liu <jl532@duke.edu>
2019-02-06 14:28:23 -0500

    init subfunction string_tolerance for extra-credit, and accompanying tests

--- tachycardia.py
+++ tachycardia.py
@@ -6,17 +6,29 @@ Created on Mon Feb  4 13:17:13 2019
 """
 import re
 
-
 def is_tachycardic(inputString):
     rmPuncInput = re.sub(r'\W', '', inputString)
     loweredInput = str.lower(rmPuncInput)
-    if loweredInput == "tachycardic":
+    targetString = "tachycardic"
+    if loweredInput == targetString: # if literally found without punctuation
         output = True
     else:
-        output = False
+        output = string_tolerance(loweredInput,targetString)
     return output
 
-
+def string_tolerance(inputString, targetString):
+    # identify missing characters
+    if len(inputString) > len(targetString):
+        return False
+    else: # now, all we test are inputStrings that are the same lenth or less than targetString
+        # diff the sets 
+        diffLists = (list(set(targetString) - set(inputString))) 
+        if len(diffLists) < 5: # now, there should be at most 2 mispellings and/or 2 missing chars, so 4 differences at most
+            # if there are 4 or less differences, now there needs to be a comparison of the order of characters
+            return True
+        else:
+            return False
+        
 def main():
     pass
 
--- test_tachycardia.py
+++ test_tachycardia.py
@@ -18,6 +18,9 @@ import pytest
         (" , tachycardic , ", True),
         (" Jason Liu", False),
         (" tachycrdic ", True),
+        (" achycardic ", True),
+        (" T4chycardic", True),
+        (" Tacycard1c", True),
 ])
 
 
@@ -26,3 +29,22 @@ def test_is_tachycardic(inputString, expected):
 
     isTachycardicTest = is_tachycardic(inputString)
     assert isTachycardicTest == expected
+    
+@pytest.mark.parametrize("testString, targetString, expected", [
+        ("jason liu", "tachycardic", False),
+        ("tachycrdic", "tachycardic", True),
+        ("achycardic", "tachycardic", True),
+        ("t4chycardic", "tachycardic", True),
+        ("tachycard1c", "tachycardic", True),
+        ("t4chycard1c", "tachycardic", True),
+        ("tchycardc", "tachycardic", True),
+        ("tachycard", "tachycardic", True),        
+        ("cidracyhcat", "tachycardic", False)
+])
+    
+    
+def test_string_tolerance(testString, targetString, expected):
+    from tachycardia import string_tolerance
+    
+    string_tolerance_test = string_tolerance(testString, targetString)
+    assert string_tolerance_test == expected
